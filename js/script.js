4d95492e89b39c0bf91a650c486f7df96a33c76b
Davide Tavella <tavella01@gmail.com>
2021-10-26 15:04:25 +0200

    commenti

--- js/script.js
+++ js/script.js
@@ -38,26 +38,31 @@ const mailClasse = ['Anthony@gmail.com',
 let mailUtente = prompt("Inserire un Email per giocare a dadi");
 
 if (mailClasse.includes(mailUtente)) {
+   // verifico che l'email sia presente nella lista
    document.getElementById("risposta").innerHTML =
    "L'email " + mailUtente + " è valida";
    document.getElementById("textgame").innerHTML =
    "Puoi finalmente giocare a dadi! Buona fortuna!";
 
+   // numero utente
    let sceltaDadoUtente = Math.floor(Math.random()*6+1);
    console.log(sceltaDadoUtente);
    document.getElementById("numeroutente").innerHTML = 
    "Il tuo numero è: " + sceltaDadoUtente;
 
+   // numero pc
    let sceltaDadoPc = Math.floor(Math.random()*6+1);
    console.log(sceltaDadoPc);
    document.getElementById("numeropc").innerHTML = 
    "Il numero del computer è: " + sceltaDadoPc;
 
+   // vince l'utente
    if (sceltaDadoUtente > sceltaDadoPc) {
       document.getElementById("vincitore").innerHTML = 
       "Complimenti, hai vinto! Il tuo " + sceltaDadoUtente + " ha battuto il " + sceltaDadoPc + " del computer.";
    }
-
+   
+   // vince il computer
    else if(sceltaDadoUtente < sceltaDadoPc) {
       document.getElementById("vincitore").innerHTML = 
       "Peccato, hai perso! Il computer con il suo " + sceltaDadoPc + " ha battuto il tuo " + sceltaDadoPc + ". Ritenta sarai più fortunato.";
@@ -66,6 +71,7 @@ if (mailClasse.includes(mailUtente)) {
 }
 
 else {
+   // l'email non c'è nella lista, non può giocare!
    document.getElementById("risposta").innerHTML =
    mailUtente + " NON è valida";
 
